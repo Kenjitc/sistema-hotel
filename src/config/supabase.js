2e1f8b6cddf22b900e42762d59bf30d3656db3d4
kenjitc <kenjitrujillomkt@gmail.com>
2026-03-09 12:47:55 -0500

    nuevo 2

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'TU_URL_DE_SUPABASE'; // Ej: [https://xyz.supabase.co](https://xyz.supabase.co)
-const supabaseKey = 'TU_ANON_KEY_DE_SUPABASE';
+const supabaseUrl = 'https://crxjtyccahkakhgyuuez.supabase.co'; // Ej: [https://xyz.supabase.co](https://xyz.supabase.co)
+const supabaseKey = 'sb_publishable_PRyoZ4IEdbJn1iv-7WAowg_hdVFLBeg';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
