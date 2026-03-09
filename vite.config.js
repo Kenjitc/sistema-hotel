e4c8e16131f1540d1c53f94b7d17f78c2ad22512
kenjitc <kenjitrujillomkt@gmail.com>
2026-03-09 13:26:15 -0500

    v3

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/hotel/',
+  plugins: [react()],
+  base: '/', // <-- Asegúrate de que esto sea solo un slash '/' o bórralo por completo.
 })
